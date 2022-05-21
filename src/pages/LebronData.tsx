88d111cdc72796a24fda5187505277f0833f10b4
insung1939 <insungcho1939@gmail.com>
2022-05-21 12:56:54 +0900

    finish!!

--- src/pages/LebronData.tsx
+++ src/pages/LebronData.tsx
@@ -1,31 +1,41 @@
-import { useEffect, useState, useCallback } from "react"
+import { useEffect, useState } from "react"
 import '../styles/LebronData.css'
-interface matchData {
-    id: number,
-    ast: number,
-    blk: number,
-    pts: number,
-    reb: number,
-    stl: number,
-    game: game
-}
-interface game {
-    date: string
-}
 const LebronData = () => {
-    let [matchdata, setMatchData] = useState([]);
+    interface dataType {
+        id: number,
+        pts: number,
+        ast: number,
+        reb: number,
+        stl: number,
+        blk: number,
+        game: game
+    }
+    interface game {
+        date: string
+    }
+    let [matchdata, setMatchData] = useState<dataType[]>([]);
     let [page, setPage] = useState(1);
     let [pageLength, setPageLength] = useState(1)
-    const getLebronData = useCallback(async () => {
-        const res = await (await fetch(`https://www.balldontlie.io/api/v1/stats?player_ids[]=237&page=${page}`)).json();
-        setMatchData(res.data)
-        setPageLength(res.meta.total_pages)
-        console.log('matchData', matchdata)
-        console.log('pageLength', pageLength)
-    }, [])
     useEffect(() => {
-        getLebronData();
-    }, [getLebronData]);
+        const getLebronData = async () => {
+            const res = await (await fetch(`https://www.balldontlie.io/api/v1/stats?player_ids[]=237&page=${page}`)).json();
+            setMatchData(res.data)
+            setPageLength(res.meta.total_pages)
+            if (pageLength === 1) {
+                console.log('data not fetching yettt')
+            } else {
+                console.log('success')
+                console.log('pageLength', pageLength)
+            }
+            if (matchdata.length === 0) {
+                console.log('not yet..')
+            } else {
+                console.log('success')
+                console.log('matchData', matchdata)
+            }
+        }
+        getLebronData()
+    }, [page])
     const showLebronMatchData = (params: number) => {
         setPage(params)
     }
@@ -58,16 +68,18 @@ const LebronData = () => {
     return (
         <div>
             <h2>Daily Match Data</h2>
-            {matchdata.map(({ id, ast, pts, reb, stl }) => (
-                <div key={id} className="matchDataContainer">
-                    <h3>득점: {pts}</h3>
-                    <h3>도움: {ast}</h3>
-                    <h3>리바운드: {reb}</h3>
-                    <h3>스틸: {stl}</h3>
-                    {/* <h3>블록: {blk}</h3>
-                    <h3>TIME: {game.date}</h3> */}
-                </div>
-            ))}
+            <div className="matchDataContainer">
+                {matchdata.map((el) => (
+                    <div key={el.id} className="matchDataBox">
+                        <h3 className="dataText date">{el.game.date.slice(0, 10)}</h3>
+                        <h3 className="dataText">득점: {el.pts}</h3>
+                        <h3 className="dataText">도움: {el.ast}</h3>
+                        <h3 className="dataText">리바운드: {el.reb}</h3>
+                        <h3 className="dataText">스틸: {el.stl}</h3>
+                        <h3 className="dataText">블록: {el.blk}</h3>
+                    </div>
+                ))}
+            </div>
             <div className="pagination">
                 {page > 1 && <span onClick={() => { leftArrowFunction() }}>ᐊ</span>}
                 {showPagination()}
